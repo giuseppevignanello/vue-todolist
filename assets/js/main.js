bb2841dfdde7a32e0929d9007bd8173d5a91d073
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-30 15:01:50 +0200

    add remove function

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,10 @@
 
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
 // Bonus:
 // oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 // cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
@@ -45,5 +47,11 @@ const { createApp } = Vue
             },
         ]
       }
+    }, 
+    methods: {
+        remove(i) {
+            this.tasks.splice(i, 1)
+        }
+
     }
   }).mount('#app')
